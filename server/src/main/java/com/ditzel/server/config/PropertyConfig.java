af0d9fd8ba9b124e2cf49129e8f5b991f1892966
Allan Ditzel <allan@allanditzel.com>
2014-01-01 14:57:28 -0600

    Initial Commit of repo

---
+++ server/src/main/java/com/ditzel/server/config/PropertyConfig.java
@@ -0,0 +1,15 @@
+package com.ditzel.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Configuration class for properties.
+ *
+ * @author Allan Ditzel
+ * @since 1.0
+ */
+@Configuration
+@PropertySource("classpath:default.properties")
+public class PropertyConfig {
+}
