5a647d40ad030305c41246af7efc474a27e42340
Allan Ditzel <allan@allanditzel.com>
2014-05-03 19:19:24 -0500

    Trying to concatenate and minify app. Not quite there yet.

--- src/main/webapp/static/Gruntfile.js
+++ src/main/webapp/static/Gruntfile.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Allan Ditzel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 module.exports = function (grunt) {
 
     grunt.initConfig({
@@ -23,13 +39,26 @@ module.exports = function (grunt) {
         },
         jshint: {
             files: ["LoginApp/*.js", "DashboardApp/*.js"]
+        },
+        concat: {
+            options: {
+                // define a string to put between each file in the concatenated output
+                separator: ';'
+            },
+            dist: {
+                // the files to concatenate
+                src: ['DashboardApp/**.js', 'LoginApp/**.js'],
+                // the location of the resulting JS file
+                dest: 'dist/<%= pkg.name %>.js'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-maven');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
-    grunt.registerTask('default', ['mavenPrepare', 'jshint', 'mavenDist']);
+    grunt.registerTask('default', ['mavenPrepare', 'jshint', 'concat', 'mavenDist']);
 
 };
